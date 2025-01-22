725b54b5a6b2f27f0f9521c40f947b509d3d17c4
exorevan <exorevan13@gmail.com>
2025-01-22 21:30:49 +0300

    Add first version of argparse

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
 import argparse
+from pathlib import Path
 import typing as ty
 
+import yaml
 
-def init_model() -> ty.Any: ...
+
+if ty.TYPE_CHECKING:
+
+    class PulseTraceParser(argparse.Namespace):
+        config: Path = Path()
 
 
-def main() -> None:
-    parser = argparse.ArgumentParser(description="PulseTrace parser")
+class PulseTracer:
+    config: dict[str, ty.Any] = {}
 
-    _ = parser.add_argument("config", help="Config for model")
+    def init_model(self) -> ty.Any: ...
 
-    args = parser.parse_args()
+    def main(self) -> None:
+        parser = argparse.ArgumentParser(description="PulseTrace parser")
 
-    model: ty.Any = init_model(args.config)
+        _ = parser.add_argument("config", help="Config for model")
+
+        args: PulseTraceParser = ty.cast(PulseTraceParser, parser.parse_args())
+
+        with open(args.config, "r", encoding="utf-8") as f:
+            self.config = yaml.safe_load(f)
+
+        model: ty.Any = self.init_model()
 
 
 if __name__ == "__main__":
-    main()
+    PulseTracer().main()
